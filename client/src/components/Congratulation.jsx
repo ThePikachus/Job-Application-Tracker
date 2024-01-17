be5da983fba0ad56587784afd26256cc7066cb40
johnnorlin <johnnorlinmusic@gmail.com>
2024-01-17 15:21:54 -0600

    presentation final

--- client/src/components/Congratulation.jsx
+++ client/src/components/Congratulation.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import ECRI_icon from "../../../assets/ECRI_icon.png";
 
 function Congratulation() {
+  const ECRI_icon =
+    "https://ci3.googleusercontent.com/meips/ADKq_NYsxe95NAAsFBH3v-jqY2mFMVVS37Ds9TYEoOM9E8UuWhSh6MtQ7fCMxtbSgwm6ga3y7IwET2Es1HPd9PlY_DpJnWY15uqI2hHQL1mU1tBdbAzOOFDYr67nKMCoaPPlh0YGh3hPYG71bg=s0-d-e1-ft#https://avatars.slack-edge.com/2023-09-27/5961449257172_d4582b88609226b9e897_88.png";
   return (
     <div className="congratulation">
       <img src={ECRI_icon} id="logo" />
